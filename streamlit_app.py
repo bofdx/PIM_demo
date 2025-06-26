5930543df09e308b7421a878f8f803e7344c4875
bofdx <damonboffo@gmail.com>
2025-06-27 08:15:06 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,23 @@ import streamlit as st
 import pandas as pd
 import os
 
+from st_pages import add_page_title, get_nav_from_toml
+
+
+
 # Set up our app
-st.set_page_config(page_title="Metadata Loader", layout='wide')
+#st.set_page_config(page_title="Metadata Loader", layout='wide')
+
+st.set_page_config(layout='wide')
+
+sections = st.sidebar.toggle('Sections', value = True, key = 'use_sections')
+
+nav = get_nav_from_toml(".streamlit/pages_sections.toml" if sections else ".streamlit/pages.toml")
+
+
 st.title("Metadata Loader")
 st.write("For uploading metadata into the database using the Excel template")
+
 
 upload_files = st.file_uploader(
     "Upload your files (CSV or Excel):",
