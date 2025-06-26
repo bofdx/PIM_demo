de3fb8a13c5d16bea82b6768fe6f67a7d7140dab
bofdx <damonboffo@gmail.com>
2025-06-27 08:28:08 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,4 +15,9 @@ sections = st.sidebar.toggle('Sections', value = True, key = 'use_sections')
 
 nav = get_nav_from_toml(".streamlit/pages_sections.toml" if sections else ".streamlit/pages.toml")
 
+pg = st.navigation(nav)
+
+add_page_title(pg)
+
+pg.run()
 
