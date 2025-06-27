af1eca25be318273a5a89d1ba6a5f1f56dd2f9fd
bofdx <damonboffo@gmail.com>
2025-06-27 09:46:50 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,9 @@ from st_pages import add_page_title, get_nav_from_toml
 
 st.set_page_config(layout='wide')
 
-sections = st.sidebar.toggle('Sections', value = True, key = 'use_sections')
+#sections = st.sidebar.toggle('Sections', value = True, key = 'use_sections')
 
-nav = get_nav_from_toml("pages_sections.toml" if sections else "pages.toml")
+nav = get_nav_from_toml() #"pages_sections.toml" if sections else "pages.toml")
 
 st.logo("STO LOGO.PNG")
 
