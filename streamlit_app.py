9006744c468f8f20cacb62e910a8b9ec7ee9e60f
bofdx <damonboffo@gmail.com>
2025-06-27 09:47:26 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.set_page_config(layout='wide')
 
 #sections = st.sidebar.toggle('Sections', value = True, key = 'use_sections')
 
-nav = get_nav_from_toml() #"pages_sections.toml" if sections else "pages.toml")
+nav = get_nav_from_toml("pages_sections.toml") #"pages_sections.toml" if sections else "pages.toml")
 
 st.logo("STO LOGO.PNG")
 
