58b0caf7bbb4a272f0f30ab6f891a90e6ebe0244
bofdx <damonboffo@gmail.com>
2025-06-30 11:54:24 +0930

    Update dev_chance_visuals.py

--- dev_chance_visuals.py
+++ dev_chance_visuals.py
@@ -1,6 +1,7 @@
 import sqlite3
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 if st.button("View All Data in dev_chance"):
     try:
@@ -11,3 +12,6 @@ if st.button("View All Data in dev_chance"):
         st.dataframe(full_df)
     except Exception as e:
         st.error(f"Error reading from DB: {e}")
+
+st.bar_chart(full_df,x="project", y ="p_tech", color = "odp_phase")
+
