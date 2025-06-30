988ff16313ba74eaf782092a12863d9d830be954
bofdx <damonboffo@gmail.com>
2025-06-30 12:18:16 +0930

    Update dev_chance_visuals.py

--- dev_chance_visuals.py
+++ dev_chance_visuals.py
@@ -7,6 +7,8 @@ connection = sqlite3.connect("PIM3.db")
 full_df = pd.read_sql_query("SELECT * FROM dev_chance", connection)
 connection.close()
 
+full_df[Pd] = full_df[ 'p_tech', 'p_fin', 'p_time', 'p_econ', 'p_mark', 'p_inf', 'p_ext'].mean(axis = 1)*full_df['commitment']
+
 if st.button("View All Data in dev_chance"):
     try:
         st.subheader("📊 All Data in dev_chance Table")
