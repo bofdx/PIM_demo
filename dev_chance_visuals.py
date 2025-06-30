04c022485d031c7128e435ac2f39149eeb816824
bofdx <damonboffo@gmail.com>
2025-06-30 16:19:01 +0930

    Update dev_chance_visuals.py

--- dev_chance_visuals.py
+++ dev_chance_visuals.py
@@ -11,16 +11,13 @@ cols = ['p_tech', 'p_fin', 'p_time', 'p_econ', 'p_mark', 'p_inf', 'p_ext']
 full_df['pd'] = full_df[cols].mean(axis=1) * full_df['commitment']
 full_df['p_min'] = full_df[cols].min(axis=1)
 
-if st.button("Load Chance of Development Data"):
-    try:
-        st.subheader("Chance of Development Full Table")
-        st.dataframe(full_df)
-    except Exception as e:
-        st.error(f"Error reading from DB: {e}")
-
 if full_df.empty:
     st.write("No Data To Display")
 else:
+    
+    st.subheader("Chance of Development Full Table")
+    st.dataframe(full_df)
+    
     st.subheader("Probability of Development")
     st.bar_chart(full_df,x="project", y ="pd", color = "odp_phase", height = 600, x_label = "Project", y_label = "Probability Development ", use_container_width=True)
 
