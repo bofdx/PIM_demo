0be2a7560c99df70d6289c846cdc26beeb366fb0
bofdx <damonboffo@gmail.com>
2025-06-30 12:21:21 +0930

    Update dev_chance_visuals.py

--- dev_chance_visuals.py
+++ dev_chance_visuals.py
@@ -8,7 +8,7 @@ full_df = pd.read_sql_query("SELECT * FROM dev_chance", connection)
 connection.close()
 
 cols = ['p_tech', 'p_fin', 'p_time', 'p_econ', 'p_mark', 'p_inf', 'p_ext']
-full_df['Pd'] = full_df[cols].mean(axis=1) * full_df['commitment']
+full_df['pd'] = full_df[cols].mean(axis=1) * full_df['commitment']
 
 if st.button("View All Data in dev_chance"):
     try:
@@ -17,6 +17,6 @@ if st.button("View All Data in dev_chance"):
     except Exception as e:
         st.error(f"Error reading from DB: {e}")
 
-st.subheader("Probability of Technical Success")
-st.bar_chart(full_df,x="project", y ="p_tech", color = "odp_phase", height = 600, x_label = "Project", y_label = "Probability of Technical Succes", use_container_width=True)
+st.subheader("Probability of Development")
+st.bar_chart(full_df,x="project", y ="pd", color = "odp_phase", height = 600, x_label = "Project", y_label = "Probability Development ", use_container_width=True)
 
