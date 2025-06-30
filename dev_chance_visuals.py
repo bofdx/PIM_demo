b408bd01f0038480e1b9e5cc7b8e452de52ce349
bofdx <damonboffo@gmail.com>
2025-06-30 12:02:25 +0930

    Update dev_chance_visuals.py

--- dev_chance_visuals.py
+++ dev_chance_visuals.py
@@ -3,11 +3,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+connection = sqlite3.connect("PIM3.db")
+full_df = pd.read_sql_query("SELECT * FROM dev_chance", connection)
+connection.close()
+
 if st.button("View All Data in dev_chance"):
     try:
-        connection = sqlite3.connect("PIM3.db")
-        full_df = pd.read_sql_query("SELECT * FROM dev_chance", connection)
-        connection.close()
         st.subheader("📊 All Data in dev_chance Table")
         st.dataframe(full_df)
     except Exception as e:
