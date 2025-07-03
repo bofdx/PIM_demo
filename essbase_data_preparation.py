dd2cdb6281cda2b85e5a25e150c3a46de5824c16
bofdx <damonboffo@gmail.com>
2025-07-03 14:53:07 +0930

    Update essbase_data_preparation.py

--- essbase_data_preparation.py
+++ essbase_data_preparation.py
@@ -6,7 +6,7 @@ import gc
 import os
 from scipy.optimize import newton
 from io import StringIO
-
+############################### SET UP 
 # Specify the file paths
 CP_Path = "Dummy_Data.csv"
 CP_Accounts = 'Dim_Account_SPMFS.csv'
@@ -35,8 +35,21 @@ df_CP = df_CP[df_CP['Category'] != 'Category']
 
 df_CP['Period']='MTD'
 
+# Specify the columns to retain during the pivot (non-year columns)
+id_vars = ['Scenario', 'Version', 'Case', 'Asset', 'Category','Period', 'Unit', 'Basis', 'Account', 'Alias']
 
-st.write(df_CP)
+# Identify the year columns (those starting with 'CY')
+year_columns = [col for col in df_CP.columns if col.startswith('CY')]
+
+# Melt the DataFrame to make 'Year' a column and the cash flows as values
+df_essbase = df_CP.melt(id_vars=id_vars, value_vars=year_columns, 
+                             var_name='Year', value_name='Value')
+
+# Remove 'CY' from the 'Year' column and convert to integer
+df_essbase['Year'] = df_essbase['Year'].str.replace('CY', '').astype(int)
+
+
+st.write(df_essbase)
 
 
 
