e7d27bdd5b267ef0034e248156dae2d3ca2ce5c8
bofdx <damonboffo@gmail.com>
2025-07-03 14:27:23 +0930

    Update essbase_data_preparation.py

--- essbase_data_preparation.py
+++ essbase_data_preparation.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import numpy_financial as npf
+import gc
+import os
+from scipy.optimize import newton
+from io import StringIO
 
+# Load the CP CubeStringIO
+df_CP = pd.read_csv(StringIO(content), delimiter=',')
+
+
+st.print(df_CP)
