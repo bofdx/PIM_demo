0f15dc49de0fbb0079fcbdb12fdbbdcb5b67900c
bofdx <damonboffo@gmail.com>
2025-07-03 14:40:34 +0930

    Update essbase_data_preparation.py

--- essbase_data_preparation.py
+++ essbase_data_preparation.py
@@ -7,10 +7,32 @@ import os
 from scipy.optimize import newton
 from io import StringIO
 
-Datafile = "Dummy_Data.csv"
+# Specify the file paths
+CP_Path = "Dummy_Data.csv"
+CP_Accounts = 'Dim_Account_CP.txt'
+#CP_Assets = 'Dim_Asset_CP.txt'
 
 # Load the CP CubeStringIO
-df_CP = pd.read_csv(Datafile, delimiter=',')
+df_CP = pd.read_csv(CP_Path, delimiter=',')
+
+## Add in the Account Names -- NOT REQUIRED JUST YET
+# df_CP_Accounts = pd.read_csv(CP_Accounts, delimiter=',')
+# df_CP_Accounts = df_CP_Accounts.rename(columns={df_CP_Accounts.columns[1]: 'Account', df_CP_Accounts.columns[2]: 'Alias'})[['Account', 'Alias']]
+# df_CP = df_CP.merge(df_CP_Accounts, on='Account')
+
+# df_CP_Assets=pd.read_csv(CP_Assets, delimiter=',')
+# df_CP_Assets = df_CP_Assets.rename(columns={df_CP_Assets.columns[1]: 'Asset',df_CP_Assets.columns[3]: 'Asset_Alias'})[['Asset', 'Asset_Alias']]
+# df_CP = df_CP.merge(df_CP_Assets, on='Asset')
+
+# df_CP = df_CP.drop(columns=['Year_NA', 'CY2022', 'CY2023', 'Asset'])
+# df_CP = df_CP.rename(columns={'Source': 'Case', 'Alias': 'Account','Account':'Alias', 'Asset_Alias': 'Asset'})
+# df_CP['Category'] = df_CP['Category'].replace('CAT_Default', 'Default')
+
 
 
 st.write(df_CP)
+
+
+
+
+
