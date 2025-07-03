d8eec529c5062354dee2fe62de4a80af393c6c61
bofdx <damonboffo@gmail.com>
2025-07-03 15:26:01 +0930

    Update essbase_data_preparation.py

--- essbase_data_preparation.py
+++ essbase_data_preparation.py
@@ -7,13 +7,7 @@ import os
 from scipy.optimize import newton
 from io import StringIO
 
-reference_year = st.number_input(
-    "Please enter a reference year for discounting",
-    value=2025,
-    step=1,
-    format="%d",
-  
-)
+reference_year = st.number_input("Please enter a reference year for discounting", value=2025, step=1, format="%d")
 
 ############################### SET UP CUBE DATA ###################################################################################################################
 # Specify the file paths
