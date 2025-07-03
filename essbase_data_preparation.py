6364437189b2cfdd60c96b361d8fd3f15bbc1706
bofdx <damonboffo@gmail.com>
2025-07-03 14:29:39 +0930

    Update essbase_data_preparation.py

--- essbase_data_preparation.py
+++ essbase_data_preparation.py
@@ -7,8 +7,10 @@ import os
 from scipy.optimize import newton
 from io import StringIO
 
+Datafile = "Dummy_Data.csv
+
 # Load the CP CubeStringIO
-df_CP = pd.read_csv(StringIO(content), delimiter=',')
+df_CP = pd.read_csv(Datafile, delimiter=',')
 
 
 st.print(df_CP)
